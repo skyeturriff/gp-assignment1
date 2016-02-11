a9fd5f1f2d1fd19e3df685ce38f3b2e86e038514
skyeturriff <turriff.skye@gmail.com>
2016-02-11 15:17:09 -0500

    Add main menu, add level 1, add sprite movement script, add scene change script

---
+++ Snake/Assets/scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour 
+{
+    // Loads the scene with the specified sceneName
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Snake/Assets/scripts/MoveSnake.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSnake : MonoBehaviour 
+{
+    // The speed at which the snake will move
+    public float movementSpeed = 0.5f;
+
+    // The direction of movement, depending on last arrow key pressed
+    Vector3 movement;
+	
+	void Update () 
+    {
+        // Move left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            movement = Vector3.left;
+        }
+
+        // Move up
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movement = Vector3.up;
+        }
+
+        // Move right
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            movement = Vector3.right;
+        }
+        
+        // Move down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            movement = Vector3.down;
+        }
+
+        // Update position of game object
+        transform.position += movement * movementSpeed * Time.deltaTime;
+	}
+}
