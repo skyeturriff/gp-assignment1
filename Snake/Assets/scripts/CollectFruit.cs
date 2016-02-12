73a7ebe9dda2b267fa6c90fe64e436f0cf2973c9
skyeturriff <turriff.skye@gmail.com>
2016-02-11 20:34:30 -0500

    Implement score tracking

--- Snake/Assets/scripts/CollectFruit.cs
+++ Snake/Assets/scripts/CollectFruit.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CollectFruit : MonoBehaviour 
 {
-    // The camera we will querying for rendering info
-    public Camera sceneCamera;
 
-    // Holds screen size information
-    Vector2 screenBounds;   
+    public Camera sceneCamera;  // The camera we will querying for screen size info
+    Vector2 screenBounds;       // Holds screen size information 
+    public Text scoreText;      // Displays current score to user
+    int score;                  // Holds the current score
 
 	void Start () 
     {
         // Initialize screen size information
-        screenBounds.y = sceneCamera.orthographicSize;  // Half total screen height
+        screenBounds.y = sceneCamera.orthographicSize;              // Half total screen height
         screenBounds.x = (screenBounds.y * sceneCamera.aspect) / 2; // Half total screen width
 	}
 
@@ -20,12 +21,24 @@ public class CollectFruit : MonoBehaviour
     {
         if (collider.gameObject.CompareTag("Fruit"))    // If snake hits a fruit
         {
-            // Get a random position within screen bounds
-            float randomX = Random.Range(-screenBounds.x, screenBounds.x);
-            float randomY = Random.Range(-screenBounds.y, screenBounds.y);
-
-            // Repsawn fruit at random position on the creen
-            collider.gameObject.transform.position = new Vector2(randomX, randomY);
+            RespawnFruit(collider.gameObject);          // Respawn it in a new location
+            UpdateScore();                              // Update user's score
         }
+    }
+
+    void RespawnFruit(GameObject fruit)
+    {
+        // Get a random position within screen bounds
+        float randomX = Random.Range(-screenBounds.x, screenBounds.x);
+        float randomY = Random.Range(-screenBounds.y, screenBounds.y);
+
+        // Repsawn fruit at random position
+        fruit.transform.position = new Vector2(randomX, randomY);
+    }
+
+    void UpdateScore()
+    {
+        ++score;                                        // Increment user's score
+        scoreText.text = "Score: " + score.ToString();  // Update score display text
     }
 }
